6cd2df8e54b611ca7bfc3d94371de2345219c445
EmptyEXOT <sadullaev.alim@mail.ru>
2022-07-02 14:57:45 +0300

    first verison of slider

--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
-/*the main js file
- *
- * You should import from ./scripts directory here!
- *
- */
-console.log('Root script!');
+const sliderDOM = document.getElementById("slider");
+const slides = document.querySelectorAll('.slide');
+const btnNext = document.getElementById('slide-next');
+const btnPrev = document.getElementById('slide-prev');
+
+function sliderInitCheck(sliderCount, sliderActiveCount, sliderPos, sliderLastPos) {
+    let errorsArr = [];
+    if (sliderPos>sliderLastPos || sliderPos<0) errorsArr.push("'sliderPos' should be less or equal then 'sliderLastPos' and more or equal '0'");
+    if (sliderActiveCount>sliderCount) errorsArr.push ("'SliderActiveCount should be less or equal 'sliderCount'");
+    if (errorsArr.length==0) return 0;
+    else return errorsArr;
+}
+
+function sliderBtnInit(sliderPos, sliderLastPos) {
+    if (sliderPos==0) {
+        return "disablePrev";
+    }
+    else if (sliderPos==sliderLastPos) {
+        return "disableNext";
+    }
+    else return 0;
+}
+
+let initializedSlider = function sliderInitialize() {
+    let mySlider = {
+        slidesCount: slides.length,
+        slidesActiveCount: 3,
+        sliderPos: 0,
+    }
+    mySlider.sliderLastPos = mySlider.slidesCount-mySlider.slidesActiveCount;
+
+    const isChecked = sliderInitCheck(mySlider.slidesCount, mySlider.slidesActiveCount, mySlider.sliderPos, mySlider.sliderLastPos);
+    if (!isChecked) {
+        sliderBtnInit(mySlider.sliderPos, mySlider.sliderLastPos);
+        console.log("initialized!");
+        return mySlider;
+    }
+    else {
+        for (let i=0; i<isChecked.length; i++) {
+            console.log(isChecked[i]);
+        }
+    }
+
+}
