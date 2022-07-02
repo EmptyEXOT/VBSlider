b147035cd8444b010ffc5223ccd7864574dfb536
EmptyEXOT <sadullaev.alim@mail.ru>
2022-07-02 14:26:14 +0300

    VBSlider initializing with min setup

---
+++ src/main.js
@@ -0,0 +1,6 @@
+/*the main js file
+ *
+ * You should import from ./scripts directory here!
+ *
+ */
+console.log('Root script!');
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: "./src/main.js"
+    },
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+                template: "./src/index.html",
+            }
+        ),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        port: 3000,
+        watchContentBase: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: ['@svgr/webpack'],
+            },
+        ]
+    }
+}
