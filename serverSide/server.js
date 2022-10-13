337196cc4b89848bc7aac3d94b9204d16b767a23
Alim <sadullaev.alim@mail.ru>
2022-10-13 20:11:04 +0300

    index changes

--- serverSide/server.js
+++ serverSide/server.js
@@ -1,30 +1,38 @@
 const http = require('http');
 const nodemailer = require('nodemailer');
 const express = require('express');
+require('dotenv').config();
 
-let app = express();
+const btn = document.getElementById('formBtn');
 
+const inputArea = document.getElementById('inputArea');
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'alimsadullaev18@gmail.com',
-        pass: 'sxgnmljreanslqfo',
+        pass: process.env.NODEMAILER_PASS,
     },
 });
 
 let mailOptions = {
     from: 'alimsadullaev18@gmail.com',
-    to: 'maratsadullaev@mail.ru',
+    to: 'sadullaev.alim@mail.ru',
     subject: 'html test message',
-    text: 'nothing',
-    html: '<div>' + '<b>hello, Marat</b>' +
-        '</div>'
+    text: undefined,
 };
 
-transporter.sendMail(mailOptions, (err)=>{
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('mail has been sent successfully');
-    };
-})
+inputArea.onchange = () => {
+    mailOptions.text = inputArea.innerText;
+    console.log(mailOptions.text);
+}
+
+
+btn.onclick = () => {
+    transporter.sendMail(mailOptions, (err)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('mail has been sent successfully');
+        };
+    })
+}
